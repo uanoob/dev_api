09e3b37b76e77628a43cd2d9e7af4e50ced115fa
uadev <usnet@ukr.net>
2019-11-24 10:53:33 +0200

    Added bootcamp routes and controllers

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const bootcamps = require('./routes/bootcamps');
 
 dotenv.config({ path: './.env' });
 
 const app = express();
+
+app.use('/api/v1/bootcamps', bootcamps);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
